8da2965f01d68539ef4c6ba7cb858f2e937bd86d
Schlueter <b@schlueter.blue>
2019-10-21 11:29:50 -0700

    Basic functionality copied from simple theme; some wip

--- js/script.js
+++ js/script.js
@@ -1,56 +1,64 @@
-// Functions for the greeter
+/* global lightdm */
+
+/*
+ * Functions for the greeter
+ * These are documented in the lightdm-webkit2-greeter(1) man page
+ */
 window.show_prompt = (text, type) => {
-	// type is either "text" or "password"
-	let prompt = document.getElementById("prompt"),
-		entry = document.getElementById("entry");
+    // type is either "text" or "password"
+    let entry = document.getElementById("entry");
+    entry.placeholder = text.charAt(0).toUpperCase() +
+        text.slice(1).replace(":", "");
 
-	entry.placeholder = text.charAt(0).toUpperCase() + text.slice(1, -1);
-
-	// clear entry
-	entry.value = "";
-	entry.type = type;
+    // clear entry
+    entry.value = "";
+    entry.type = type;
 };
 
 window.show_message = (text, type) => {
-	if (0 === text.length) {
-		return;
-	}
-	let messages = document.getElementById('messages');
-	messages.style.visibility = "visible";
-	// type is either "info" or "error"
-	if (type == 'error') {
-		text = `<p style="color:red;">${text}</p>`;
-	}
-	messages.innerHTML = `${messages.innerHTML}${text}`;
+    let messages = document.getElementById('messages');
+    messages.style.visibility = "visible";
+    // type is either "info" or "error"
+    if (type == 'error') {
+        text = `<p style="color:red;">${text}</p>`;
+    }
+    messages.innerHTML = `${messages.innerHTML}${text}`;
 };
 
 window.authentication_complete = () => {
-	if (lightdm.is_authenticated) {
-		// Start default session
-		// let body = document.getElementById('body');
-		document.documentElement.addEventListener('transitionend', () => {lightdm.start_session_sync('xmonad')});
-		document.documentElement.className = 'session_starting';
-	} else {
-		show_message("Authentication Failed", "error");
-		setTimeout(start_authentication, 3000);
-	}
+    if (lightdm.is_authenticated) {
+        document.documentElement.addEventListener(
+            'transitionend',
+            () => {lightdm.start_session_sync('xmonad')}
+        );
+        document.documentElement.className = 'session_starting';
+    } else {
+        window.show_message("Authentication Failed", "error");
+        setTimeout(window.start_authentication, 3000);
+    }
 };
 
-autologin_timer_expired = (username) => {}
-
-// Theme Functions
-clear_messages = () => {
-	let messages = document.getElementById("messages");
-	messages.innerHTML = "";
-	messages.style.visibility = "hidden";
+/*
+ * Theme Functions
+ */
+window.clear_messages = () => {
+    let messages = document.getElementById("messages");
+    messages.innerHTML = "";
+    messages.style.visibility = "hidden";
 }
 
 window.start_authentication = () => {
-	clear_messages();
-	lightdm.authenticate();
+    window.clear_messages();
+    lightdm.authenticate();
+    window.show_message("Hello");
+    window.display_session_choices();
 };
 
 window.handle_input = () => {
-	let entry = document.getElementById("entry");
-	lightdm.respond(entry.value);
+    let entry = document.getElementById("entry");
+    lightdm.respond(entry.value);
 };
+
+window.display_session_choices = () => {
+    window.show_message(lightdm.sessions);
+}
